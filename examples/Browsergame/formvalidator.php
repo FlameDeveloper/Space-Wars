42791345580a0c8c6db90d4ebbd5946e4c25dbce
FlameDeveloper <Frozen@Frozen-PC.(none)>
2012-03-25 17:56:44 +0100

    css bearbeitet - signup_check

--- examples/Browsergame/formvalidator.php
+++ examples/Browsergame/formvalidator.php
@@ -5,70 +5,70 @@ $attributes = array(
         "valid" => true,
         "msg" => array()
     ),
-    "lastname"  => array(
+    "lastname" => array(
         "valid" => true,
         "msg" => array()
     ),
-    "username"  => array(
-    "valid" => true,
-    "msg" => array()
+    "username" => array(
+        "valid" => true,
+        "msg" => array()
     ),
-    "password"  => array(
-    "valid" => true,
-    "msg" => array()
+    "password" => array(
+        "valid" => true,
+        "msg" => array()
     ),
-    "birth_day"  => array(
-    "valid" => true,
-    "msg" => array()
+    "birth_day" => array(
+        "valid" => true,
+        "msg" => array()
     ),
-    "birth_month"  => array(
-    "valid" => true,
-    "msg" => array()
+    "birth_month" => array(
+        "valid" => true,
+        "msg" => array()
     ),
-    "birth_year"  => array(
-    "valid" => true,
-    "msg" => array()
+    "birth_year" => array(
+        "valid" => true,
+        "msg" => array()
     ),
-    "sex"  => array(
-    "valid" => true,
-    "msg" => array()
+    "sex" => array(
+        "valid" => true,
+        "msg" => array()
     ),
-    "area_code"  => array(
-    "valid" => true,
-    "msg" => array()
+    "area_code" => array(
+        "valid" => true,
+        "msg" => array()
     ),
-    "mobile"  => array(
-    "valid" => true,
-    "msg" => array()
+    "mobile" => array(
+        "valid" => true,
+        "msg" => array()
     ),
-    "email"  => array(
-    "valid" => true,
-    "msg" => array()
+    "email" => array(
+        "valid" => true,
+        "msg" => array()
     ),
-    "location"  => array(
-    "valid" => true,
-    "msg" => array()
+    "location" => array(
+        "valid" => true,
+        "msg" => array()
     ),
-    "terms_of_service"  => array(
-    "valid" => true,
-    "msg" => array()
+    "terms_of_service" => array(
+        "valid" => true,
+        "msg" => array()
     )
 );
 
 
-$error= false;
-if($_POST) {
+$error = false;
+if ($_POST) {
 
     $firstname = $_POST['firstname'];
     $firstname_length = strlen($firstname); //kk
-    if(!(3 <= $firstname_length and $firstname_length <= 30)) {
+    if (!(3 <= $firstname_length and $firstname_length <= 30)) {
         $attributes["firstname"]["valid"] = false;
         array_push($attributes["firstname"]["msg"], 'Die Eingabe des Vornamens ist nicht nachvollziehbar.');
         $error = true;
     }
     $lastname = $_POST['lastname'];
     $lastname_length = strlen($lastname); //kk
-    if(!(3 <= $lastname_length and $lastname_length <= 30)) {
+    if (!(3 <= $lastname_length and $lastname_length <= 30)) {
         $attributes["lastname"]["valid"] = false;
         array_push($attributes["lastname"]["msg"], 'Die Eingabe des Nachnamens ist nicht nachvollziehbar.');
         $error = true;
@@ -76,9 +76,9 @@ if($_POST) {
 
     $username = $_POST['username'];
     $username_length = strlen($username);
-    if(!(4 <= $username_length and $username_length <= 30)) {
-    $attributes["username"]["valid"] = false;
-    array_push($attributes["username"]["msg"], 'Die Eingabe des Benutzernamen hat die falsche Länge.');
+    if (!(4 <= $username_length and $username_length <= 30)) {
+        $attributes["username"]["valid"] = false;
+        array_push($attributes["username"]["msg"], 'Die Eingabe des Benutzernamen hat die falsche Länge.');
         $error = true;
     }
 
@@ -87,61 +87,62 @@ if($_POST) {
     //$attributes["username"]["valid"] = false;
     //array_push($attributes["username"]["msg"], 'Der Username ist bereits vergeben.');
 
-    if(!($_POST['password'] == $_POST['password_confirmation']
-       and "" != $_POST['password'])) {
-    $attributes["password"]["valid"] = false;
-    array_push($attributes["password"]["msg"], 'Die Eingabe des Passwortes ist nicht nachvollziehbar.');
+    if (!($_POST['password'] == $_POST['password_confirmation']
+        and "" != $_POST['password'])
+    ) {
+        $attributes["password"]["valid"] = false;
+        array_push($attributes["password"]["msg"], 'Die Eingabe des Passwortes ist nicht nachvollziehbar.');
         $error = true;
     }
 
     $birth_day = (Integer)$_POST['birth_day'];
-    if(!(1 <= $birth_day and $birth_day <= 31)) {
-    $attributes["birth_day"]["valid"] = false;
-    array_push($attributes["birth_day"]["msg"], 'Die Eingabe des Geburtstages ist nicht nachvollziehbar.');
-    $error = true;
+    if (!(1 <= $birth_day and $birth_day <= 31)) {
+        $attributes["birth_day"]["valid"] = false;
+        array_push($attributes["birth_day"]["msg"], 'Die Eingabe des Geburtstages ist nicht nachvollziehbar.');
+        $error = true;
     }
 
     $birth_month = (Integer)$_POST['birth_month'];
-    if(!(1 <= $birth_month and $birth_month <= 12)) {
-    $attributes["birth_month"]["valid"] = false;
-    array_push($attributes["birth_month"]["msg"], 'Die Eingabe des Geburtsmonates ist nicht nachvollziehbar.');
+    if (!(1 <= $birth_month and $birth_month <= 12)) {
+        $attributes["birth_month"]["valid"] = false;
+        array_push($attributes["birth_month"]["msg"], 'Die Eingabe des Geburtsmonates ist nicht nachvollziehbar.');
         $error = true;
     }
 
     $birth_year = (Integer)$_POST['birth_year'];
     if (!(1900 <= $birth_year and $birth_year <= 2012)) {
-    $attributes["birth_year"]["valid"] = false;
-    array_push($attributes["birth_year"]["msg"], 'Die Eingabe des Geburtsjahres ist nicht nachvollziehbar.');
+        $attributes["birth_year"]["valid"] = false;
+        array_push($attributes["birth_year"]["msg"], 'Die Eingabe des Geburtsjahres ist nicht nachvollziehbar.');
         $error = true;
     }
 
     $sex = $_POST['sex'];
-    if(!('male' == $sex or 'female' == $sex or '' == $sex)) {
-    $attributes["sex"]["valid"] = false;
-    array_push($attributes["sex"]["msg"], 'Die Eingabe des Geschlechts ist nicht nachvollziehbar.');
+    if (!('male' == $sex or 'female' == $sex or '' == $sex)) {
+        $attributes["sex"]["valid"] = false;
+        array_push($attributes["sex"]["msg"], 'Die Eingabe des Geschlechts ist nicht nachvollziehbar.');
         $error = true;
     }
 
     $email = $_POST['email'];
     $email_length = strlen($email);
-    if(!(8 <= $email_length and $email_length <= 50)) {
-    $attributes["email"]["valid"] = false;
-    array_push($attributes["email"]["msg"], 'Die Eingabe der E-mail ist nicht nachvollziehbar.');
+    if (!(8 <= $email_length and $email_length <= 50)) {
+        $attributes["email"]["valid"] = false;
+        array_push($attributes["email"]["msg"], 'Die Eingabe der E-mail ist nicht nachvollziehbar.');
         $error = true;
     }
 
-   // $terms_of_service = $_POST['terms_of_service'];
-   // $terms_of_service = $terms_of_service == 'on';
-   // if(!($terms_of_service)) {
-   // $attributes["terms_of_service"]["valid"] = false;
-   // array_push($attributes["terms_of_service"]["msg"], 'Die Eingabe der Nutzuungsbedingungen ist nicht nachvollziehbar.');
-   //     $error = true;
-   // }
+    // $terms_of_service = $_POST['terms_of_service'];
+    // $terms_of_service = $terms_of_service == 'on';
+    // if(!($terms_of_service)) {
+    // $attributes["terms_of_service"]["valid"] = false;
+    // array_push($attributes["terms_of_service"]["msg"], 'Die Eingabe der Nutzuungsbedingungen ist nicht nachvollziehbar.');
+    //     $error = true;
+    // }
 
     //foreach($attributes as $name => $attribute) // lol
     //    if($attribute["valid"] == false )
     //        $error = true;
-    if($error) {    // so
+    if ($error) { // so
         require 'signup.php';
     } else {
         require 'signup_check.php';
--- examples/Browsergame/signup_check.php
+++ examples/Browsergame/signup_check.php
@@ -8,9 +8,6 @@
 //    print_r($_POST);
 //}
 
-require "formvalidator.php";
-
-
 
 $birth_month = (int)$_POST['birth_month'];
 $months = array(
@@ -31,40 +28,54 @@ $months = array(
 ?>
 <!doctype html>
 <html>
-<head>
-    <title>Neues Life-Konto erstellen</title>
-    <link rel="stylesheet" type="text/css" href="style.css" />
-</head>
-<body>
-<div id="form_check">
-    <h1>&Uuml;berpr&uuml;fung der Daten</h1>
-    <dl>
-        <dt>Vorname</dt>
-        <dd><?= $_POST['firstname'] ?></dd>
-        <dt>Nachname</dt>
-        <dd><?= $_POST['lastname'] ?></dd>
-        <dt>Benutzername</dt>
-        <dd><?= $_POST['username'] ?></dd>
-        <dt>Geburtstag</dt>
-        <dd><?= $_POST['birth_day'] ?>.
-            <?= $months[$birth_month-1] ?>
-            <?= $_POST['birth_year'] ?></dd>
-        <dt>Geschlecht</dt>
-        <dd><?= $_POST['sex'] ?></dd>
-        <dt>E-Mail-Adresse</dt>
-        <dd><?= $_POST['email'] ?></dd>
-        <dt>Nutzungsbedingungen</dt>
-        <dd>
-            <? if($_POST['terms_of_service'] == "on") { ?>
-            Akzeptiert
-            <? } else { ?>
-            Nicht Akzeptiert
-            <? } ?>
-        </dd>
-    </dl>
-    <input type="submit" value="Bestätigen">
-</div>
-</body>
+    <head>
+        <title>Space Wars verification</title>
+        <link rel="stylesheet" type="text/css" href="css/style2.css"/>
+    </head>
+    <body>
+        <img src="images/space_wars.jpg" alt="background image" id="bg"/>
+
+        <div id="content">
+            <div id="headline">Space Wars</div>
+            <div id="form_check">
+                <h1>&Uuml;berpr&uuml;fung der Daten</h1>
+                <dl>
+                    <dt>Vorname</dt>
+                    <dd><?= $_POST['firstname'] ?></dd>
+                    <dt>Nachname</dt>
+                    <dd><?= $_POST['lastname'] ?></dd>
+                    <dt>Benutzername</dt>
+                    <dd><?= $_POST['username'] ?></dd>
+                    <dt>Geburtstag</dt>
+                    <dd><?= $_POST['birth_day'] ?>.
+                        <?= $months[$birth_month - 1] ?>
+                        <?= $_POST['birth_year'] ?></dd>
+                    <dt>Geschlecht</dt>
+                    <dd><?= $_POST['sex'] ?></dd>
+                    <dt>E-Mail-Adresse</dt>
+                    <dd><?= $_POST['email'] ?></dd>
+                    <dt>Nutzungsbedingungen</dt>
+                    <dd>
+                        <? if ($_POST['terms_of_service'] == "on") { ?>
+                        Akzeptiert
+                        <? } else { ?>
+                        Nicht Akzeptiert
+                        <? } ?>
+                    </dd>
+                </dl>
+                <form action="">
+                <input type="hidden" name="firstname" value="$_POST['firstname']">
+                <input type="hidden" name="lastname" value="$_POST['lastname']">
+                <input type="hidden" name="username" value="$_POST['username']">
+                <input type="hidden" name="birth_day" value="$_POST['birth_day']">
+                <input type="hidden" name="birth_year" value="$_POST['birth_year']">
+                <input type="hidden" name="sex" value="$_POST['sex']">
+                <input type="hidden" name="email" value=$_POST['email']>
+                <input type="submit" value="Best&auml;tigen">
+                </form>
+            </div>
+        </div>
+    </body>
 </html>
 
 <?php
